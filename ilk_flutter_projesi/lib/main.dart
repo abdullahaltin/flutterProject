2aee77565b8c25b84e4a2e2d1139f15634f6c27e
abdullahaltin <56593188+abdullahaltin@users.noreply.github.com>
2020-09-02 03:15:12 +0300

    Temel widget'lar sayac ornegi

--- ilk_flutter_projesi/lib/main.dart
+++ ilk_flutter_projesi/lib/main.dart
@@ -7,124 +7,112 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        /*textTheme: TextTheme(
+          display2: TextStyle(fontSize:12),
+        ),*/
         primaryColor: Colors.blue,
       ),
       title: "MaterialApp Title",
-      home: MyHomePage(),
+      home: MyHomePage(title: "My Home Page 2"),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  final String title;
+  int sayac = 0;
+
+  MyHomePage({this.title}) {
+    debugPrint("MyHomePage statefull widget contructor");
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    debugPrint("MyHomePage create state tetiklendi");
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState() {
+    debugPrint("MyHomePageState contructor tetiklendi");
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint("MyHomePageState build metodu tetiklendi");
     return Scaffold(
       appBar: AppBar(
-        title: Text("MyHomePage"),
+        // parmetrenizi elle vermek yerine üst sınıftan isimlendirilmiş contructor ile gönderdik
+        title: Text(widget.title),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            _sayacDegeriniArtir();
+            debugPrint("sayac degeri arttırıldı set state cagrıldı");
+          });
+        },
         child: Icon(Icons.add),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Butona Basılma Sayısı"),
+            RaisedButton(
+              onPressed: () {
+                _sayacDegeriniArtir();
+              },
+              child: Text("Artır"),
+              color: Colors.yellow,
+            ),
             Text(
-              "5",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              "${widget.sayac}",
+              style: Theme.of(context).textTheme.display2.copyWith(
+                    //copyWith - ile bir üst sınıftan aldığımız değerleri değiştirme yapabiliriz
+                    color: widget.sayac <= 0 ? Colors.red : Colors.green,
+                  ),
+            ),
+            RaisedButton(
+              onPressed: _sayacDegeriniAzalt,
+              child: Text(
+                "Azalt",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.red,
+            ),
+            FlatButton(
+              onPressed: _sayacDegeriniSifirla,
+              child: Text(
+                "Reset",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.purple,
             )
           ],
         ),
       ),
     );
   }
-}
 
-/*
-import 'package:flutter/material.dart';
-import 'ui/my_app.dart';
-//import 'package:ilk_flutter_projesi/ui/resin_ve_buton_turleri.dart';
-//import 'ui/resim_ve_buton_turleri.dart';
-//bu şekilde de olur, çünkü flutter çalışınca önce lib klasörü altına bakar
-
-void main(List<String> args) {
-// aşağıda kendi widget ımızı yaptık,sonra yeni bir nesne oluşturduk
-// ve runApp a myApp geçerek çalıştırdık değişen bir şey olmadı
-//var myApp = MyApp();
-// buradaki kısa yazım yukardaki new ile aynı anlama geliyor
-  runApp(MyApp());
-}
-
-// yukarıdaki yazım tarzı yerine fat arrow aşağıdaki gibide yazabiliriz
-//void main() => MyApp();
- */
-
-/*import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _sayacDegeriniArtir() {
     setState(() {
-      _counter++;
+      widget.sayac++;
+      debugPrint("Sayac degeri : ${widget.sayac}");
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+  void _sayacDegeriniAzalt() {
+    setState(() {
+      widget.sayac--;
+      debugPrint("Sayac degeri : ${widget.sayac}");
+    });
+  }
+
+  void _sayacDegeriniSifirla() {
+    setState(() {
+      widget.sayac = 0;
+      debugPrint("Sayac degeri : ${widget.sayac}");
+    });
   }
 }
- */
--- s____il/Sil/ilk_flutter_projesi/android/app/src/main/kotlin/com/example/ilk_flutter_projesi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ilk_flutter_projesi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
